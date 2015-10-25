3a371a55dd041dad5c8a46bb895e4925eaba45f6
lizbachmann <lbachmann06@gmail.com>
2015-10-25 16:29:47 -0500

    js

--- myjavascript.js
+++ myjavascript.js
@@ -55,3 +55,76 @@ function countries(europe, asia, africa, america){
 var favoriteCountry = new city("Ireland", "China", "Algeria", "USA");
 var leastFavorite = new city("Spain","Japan", "Benin", "Mexico");
 
+//var person = {
+//   name: "Liz",
+//    height: "68",
+//    gender: "female",
+//    age: 24,
+//    glasses: true,
+//    lastName: "Bachamnn",
+//    getFullName: function(){
+//        return this.name + " " + this.lastName;
+//    }
+//}
+//document.write(person.getFullName());
+//
+//
+//var person = {
+//     age:  prompt("what's your age?"),
+//   getAge: function(){
+//    return this.age + "years old";
+//    }
+//}
+//
+//document.write(person.getAge());
+
+//function Person(firstName, lastName, age){
+//    this.firstName = firstName;
+//    this.lastName = lastName;
+//    this.age = age;
+//}
+//
+//var me = new Person("Liz", "Bachmann", 24)
+//
+//document.write(Person.me());
+
+//function getInfo(){
+//    var age = prompt("What's your age?");
+//    var firstName = prompt("What's your first name?");
+//    var lastName = prompt("what's your last name?");
+//    return new Person(firstName + lastName + "is " + age);
+//}
+//var visitor = getInfo();
+
+//var pageHeader = document.getElementById("header");
+//    console.log(pageHeader);
+//    pageHeader.textContent = "HOWDY";
+//
+//var greeting = document.getElementById("greeting") ;
+//    console.log(greeting);
+//    var name = prompt("What's your name?");
+//    greeting.textContent = "Hello " + name;
+////
+//var selectorOne = document.querySelector("li.blue");
+//selectorOne.textContent = "nopeeeee";
+////static items will not update
+//var selector2 = document.querySelectorAll(".blue");
+//console.log(selector2);
+//
+//
+////live item that updates as the page does
+//var selector3 = document.getElementsByClassName("blue");
+//console.log(selector3);
+//
+//var parent = selector3[0].parentElement;
+//
+//var newItem = document.createElement("li");
+//
+//var newItemText = document.createTextNode("Lana");
+//
+//newItem.appendChild(newItemText);
+//
+//newItem.className = newItem.className + " blue";
+//
+//parent.appendChild(newItem);
+//newItem.className = "blue";
